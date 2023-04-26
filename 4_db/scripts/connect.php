1b63b9c88cee1c76097675a2b677a94b920dd1d2
unknown <oskargierlicz@gmail.com>
2023-04-26 17:13:26 +0200

    update_user

--- 4_db/scripts/connect.php
+++ 4_db/scripts/connect.php
@@ -1,4 +1,5 @@
 <?php
 //	echo "db";
+	//$conn = new mysqli("localhost", "root", "", "cdv_gr_3");
 	$conn = new mysqli("localhost", "root", "", "cdv_gr_3_v3");
 //	echo $conn->connect_errno; //0 -> ok
